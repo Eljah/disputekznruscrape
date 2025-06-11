9ee1193e99ab913634302faeaa3a9d54f29639c4
Ilya <eljah@mail.ru>
2025-06-11 10:15:07 +0300

    Add selenium scraper example

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # disputekznruscrape
-disputekznruscrape
+
+Simple Maven project demonstrating how to scrape pages of the site
+`https://dispute.kzn.ru` using Selenium in headless mode.
+
+## Build
+
+```
+mvn package
+```
+
+## Run
+
+By default the program downloads the text of disputes with id from 1 to 3
+and saves them to `dispute-<id>.txt`. You can specify a different upper
+id as the first argument.
+
+```
+java -cp target/disputescraper-1.0-SNAPSHOT.jar com.example.App 5
+```
+
+This will create files `dispute-1.txt` ... `dispute-5.txt` in the working
+directory.
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            int maxId = 3;
+            if (args.length > 0) {
+                try {
+                    maxId = Integer.parseInt(args[0]);
+                } catch (NumberFormatException ignored) {}
+            }
+            for (int id = 1; id <= maxId; id++) {
+                driver.get("https://dispute.kzn.ru/disputes/" + id);
+                WebElement body = driver.findElement(By.tagName("body"));
+                String text = body.getText();
+                Files.writeString(Path.of("dispute-" + id + ".txt"), text, StandardCharsets.UTF_8);
+            }
+        } finally {
+            driver.quit();
+        }
+    }
+}
